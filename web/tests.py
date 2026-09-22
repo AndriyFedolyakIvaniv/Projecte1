054140d8509ea84d4a024206499bb43385b9ba06
AndriyFedolyakIvaniv <andruhafedoliak@gmail.com>
2026-09-22 16:18:04 +0200

    feat: add public question views

--- web/tests.py
+++ web/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Choice, Question
 
@@ -39,3 +40,48 @@ class ChoiceModelTests(TestCase):
 
 		self.assertEqual(choice.votes, 0)
 		self.assertEqual(question.choice_set.get(), choice)
+
+
+class QuestionViewTests(TestCase):
+	def test_index_displays_latest_five_questions(self):
+		for question_number in range(6):
+			Question.objects.create(
+				question_text=f'Question {question_number}',
+				pub_date=timezone.now() + datetime.timedelta(minutes=question_number),
+			)
+
+		response = self.client.get(reverse('web:index'))
+
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, 'Question 5')
+		self.assertNotContains(response, 'Question 0')
+
+	def test_index_shows_empty_message_without_questions(self):
+		response = self.client.get(reverse('web:index'))
+
+		self.assertContains(response, 'No questions are available.')
+
+	def test_detail_displays_question_and_choices(self):
+		question = Question.objects.create(
+			question_text='What is new?',
+			pub_date=timezone.now(),
+		)
+		Choice.objects.create(question=question, choice_text='Nothing')
+
+		response = self.client.get(reverse('web:detail', args=[question.pk]))
+
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, 'What is new?')
+		self.assertContains(response, 'Nothing')
+
+	def test_detail_returns_not_found_for_unknown_question(self):
+		response = self.client.get(reverse('web:detail', args=[999]))
+
+		self.assertEqual(response.status_code, 404)
+
+	def test_results_and_vote_views_include_question_id(self):
+		results_response = self.client.get(reverse('web:results', args=[7]))
+		vote_response = self.client.get(reverse('web:vote', args=[7]))
+
+		self.assertContains(results_response, 'results of question 7')
+		self.assertContains(vote_response, 'voting on question 7')
